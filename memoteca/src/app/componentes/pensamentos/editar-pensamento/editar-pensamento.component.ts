c3e422c46acc8c3ed18285e1a3b2588a168f4309
Sergio Medeiros <sergiof11@outlook.com.br>
2023-07-08 11:20:50 -0300

    UPDATE: Implementando função de editar pensamento, não concluída

--- memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-editar-pensamento',
   templateUrl: './editar-pensamento.component.html',
   styleUrls: ['./editar-pensamento.component.css']
 })
-export class EditarPensamentoComponent {
+export class EditarPensamentoComponent implements OnInit {
 
+  pensamento : Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {}
+
+    ngOnInit(): void {
+      const id = this.route.snapshot.paramMap.get('id')
+      this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+        this.pensamento = pensamento;
+      })
+
+      
+    }
 }
