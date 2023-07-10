dd7e66562cf19576e0f7b96986d59416b78ad53a
Sergio Medeiros <sergiof11@outlook.com.br>
2023-07-10 17:39:54 -0300

    FEATURE: Incluindo projeto para estudo do NXJS

---
+++ 2685-angular-rxjs-projeto-base/src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  campoBusca: string;
+  listaLivros: [];
+
+  constructor() { }
+
+}
+
+
+
