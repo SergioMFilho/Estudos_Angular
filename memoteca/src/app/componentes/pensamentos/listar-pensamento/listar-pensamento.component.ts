4d2cdb4bc837bf3e9832104ed3ac3a729a687aaa
Sergio Medeiros <sergiof11@outlook.com.br>
2023-07-19 09:02:37 -0300

    FEATURE: Listando favoritos

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,17 +12,18 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   filtro: string = ''
+  favoritos: boolean = false;
 
   constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos;
     })
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+    this.service.listar(++this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentos) => {
       this.listaPensamentos.push(...listaPensamentos)
       if(!listaPensamentos.length) {
         this.haMaisPensamentos = false;
@@ -32,15 +33,16 @@ export class ListarPensamentoComponent implements OnInit {
 
   pesquisarPensamentos() {
     this.haMaisPensamentos = true;
-    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
 
   carregarMeusFavoritos() {
-    this.haMaisPensamentos = true;
-    this.paginaAtual = 1;
-    this.service.listarPensamentosFavoritos(this.paginaAtual, this.filtro).subscribe((listaPensamentosFavoritos) => {
+    this.favoritos = true
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentosFavoritos) => {
       this.listaPensamentos = listaPensamentosFavoritos
     })
   }
