29590fa0bbaa1edd31933179a3b8396d023f1107
SergioMFilho <sergiof11@outlook.com.br>
2023-07-23 14:12:34 -0300

    UPDATE: Passando dados entre os componentes

--- life-cycle-projeto-inicial/src/app/app.component.ts
+++ life-cycle-projeto-inicial/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app-lista-de-compras';
+  listaDeCompra : Array<Item> = []
 
-  constructor() { }
+  constructor(private servicoListaCompra: ListaDeCompraService) { }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.servicoListaCompra.getListaDeCompra();
+    console.log(this.listaDeCompra)
+  }
+
 }
--- life-cycle-projeto-inicial/src/app/components/item/item.component.ts
+++ life-cycle-projeto-inicial/src/app/components/item/item.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges{
 
+  @Input() item! : Item;
   faPen = faPen;
   faTrash = faTrash
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    console.log('OnInit')
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChanges')
+  }
 
 }
