01f75b96814dae52c1437d129bd6e8a9966354be
SergioMFilho <sergiof11@outlook.com.br>
2023-07-31 21:57:18 -0300

    FEATURE: Criando arquivo ts para animações

---
+++ animacoes-projeto-base/src/app/animations.ts
@@ -0,0 +1,48 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
+
+export const highlightedStateTrigger = trigger('highlightedState', [
+    state('default', style({
+        border: '2px solid #B2B6FF',
+        scale: '1.0'
+    })),
+    state('highlighted', style({
+        border: '2px solid blue',
+        filter: 'brightness(97%)',
+        scale: '1.04'
+    })),
+    transition('* => highlighted', animate('300ms ease-in')), // estado inicial => estadoFinal,
+    transition('highlighted => *', animate('300ms ease-out'))
+])
+
+export const showStateTrigger = trigger('showState', [
+    state('show', style({})),
+    transition('void => show', [
+      style({
+        opacity: 0
+      }),
+      animate(300, style({
+        opacity: 1
+      }))
+    ]),
+    transition('show => void', animate(300, style({
+      opacity: 0
+    })))
+  ]);
+
+
+
+//   export const highlightedStateTrigger = trigger('highlightedState', [
+//     state('default', style({
+//       border: '2px solid #B2B6FF'
+//     })),
+//     state('highlighted', style({
+//       border: '4px solid #B2B6FF',
+//       filter: 'brightness(92%)'
+//     })),
+//     transition('default => highlighted', [
+//       animate('200ms ease-out', style({
+//         transform: 'scale(1.02)'
+//       })),
+//       animate(200)
+//     ])
+//   ])
