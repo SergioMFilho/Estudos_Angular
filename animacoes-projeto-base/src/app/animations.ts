06446f4a13cde5e514ce6b9afd5925abad50e756
SergioMFilho <sergiof11@outlook.com.br>
2023-08-01 11:18:31 -0300

    FEATURE: Adicionando projetos padrão

--- animacoes-projeto-base/src/app/animations.ts
+++ animacoes-projeto-base/src/app/animations.ts
@@ -19,29 +19,47 @@ export const showStateTrigger = trigger('showState', [
       style({
         opacity: 0
       }),
-      animate(300, style({
+      animate(500, style({
         opacity: 1
       }))
     ]),
-    transition(':leave', animate(300, style({    // elemento sai do DOM  ===  * => void
+    transition(':leave', animate(10, style({    // elemento sai do DOM  ===  * => void
       opacity: 0
     })))
   ]);
 
+export const showIllustrationTrigger = trigger('showIllustration', [
+    transition(':enter', [      // elemento aparece no DOM  ===  void => *
+      style({
+        opacity: 0
+      }),
+      animate(600, style({
+        opacity: 1
+      }))
+    ]),
+    transition(':leave', animate(10, style({    // elemento sai do DOM  ===  * => void
+      opacity: 0
+    })))
+  ]);
 
+export const checkButtonTrigger = trigger('checkButton', [
+    transition('* => checked', [
+        animate('400ms 200ms ease-in', style({
+            transform: 'scale(0.4)'
+        })),
+    ]),
+]);
 
-//   export const highlightedStateTrigger = trigger('highlightedState', [
-//     state('default', style({
-//       border: '2px solid #B2B6FF'
-//     })),
-//     state('highlighted', style({
-//       border: '4px solid #B2B6FF',
-//       filter: 'brightness(92%)'
-//     })),
-//     transition('default => highlighted', [
-//       animate('200ms ease-out', style({
-//         transform: 'scale(1.02)'
-//       })),
-//       animate(200)
-//     ])
-//   ])
+// export const highlightedStateTrigger = trigger('highlightedState', [
+//   state('default', style({
+//       border: '2px solid #B2B6FF',
+//       transform: 'scale(1)'
+//   })),
+//   state('highlighted', style({
+//       border: '2px solid blue',
+//       filter: 'brightness(97%)',
+//       transform: 'scale(1.04)'
+//   })),
+//   transition('* => highlighted', animate('200ms ease-in')),
+//   transition('highlighted => *', animate('200ms ease-out'))
+// ]);
--- animacoes-projeto-base/src/app/componentes/mensagem/mensagem.component.ts
+++ animacoes-projeto-base/src/app/componentes/mensagem/mensagem.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-mensagem',
   templateUrl: './mensagem.component.html',
-  styleUrls: ['./mensagem.component.css']
+  styleUrls: ['./mensagem.component.scss']
 })
 export class MensagemComponent {
 
